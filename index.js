08115383d43bff11d689164a22c1741754e89060
Wendy She <whshiwen@gmail.com>
2018-05-17 21:55:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,3 @@
+var recipes={salad:"Arugula", dinner:"duck", breakfast:"beans"}
+function updateObjectWithKeyAndValue(object,key,value){return Object.assign({},object,{[`${key}`]:`${value}`})}
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
