403be4c683a2afca992fabb321af41509b4204f0
Wendy She <whshiwen@gmail.com>
2018-05-17 21:56:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var recipes={salad:"Arugula", dinner:"duck", breakfast:"beans"}
 function updateObjectWithKeyAndValue(object,key,value){return Object.assign({},object,{[`${key}`]:`${value}`})}
-function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){object[key]=value;return (object)}
